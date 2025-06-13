34a3887fe7bd0c2892341ff1545b525cc48b4712
Yash Khokhani <theyashkhokhani@gmail.com>
2025-06-13 21:52:50 +0530

    create db models

--- source-codes/dodo/thoughts/admin.py
+++ source-codes/dodo/thoughts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Folder, ToDo, Card
 
 # Register your models here.
+admin.site.register(Folder)
+admin.site.register(ToDo)
+admin.site.register(Card)
--- source-codes/dodo/thoughts/models.py
+++ source-codes/dodo/thoughts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Folder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ToDo(models.Model):
+    COMPLETION_STATUS = [
+        ('Open', 'Pending'),
+        ('Close', 'Completed'),
+    ]
+    folder = models.ForeignKey(
+        Folder,
+        on_delete=models.CASCADE,
+        null=True)
+    tag = models.CharField(max_length=19)
+    description = models.CharField(max_length=240)
+    status = models.CharField(max_length=10, choices=COMPLETION_STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Card(models.Model):
+    folder = models.ForeignKey(
+        Folder,
+        on_delete=models.CASCADE,
+        null=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
